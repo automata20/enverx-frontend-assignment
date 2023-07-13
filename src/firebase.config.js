b9936a4ba12ce0edad2b691e8a8ebf7501666fcf
automata20 <miteshrajput.dev@gmail.com>
2023-07-13 15:41:42 +0530

    chore: minor fix

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,10 +5,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
